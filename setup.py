31a0bfb09b1b22c056b99e7eca4cdbb9492301e4
HadrienFlo <hadrien.florentin@gmail.com>
2025-10-26 15:11:26 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r", encoding="utf-8") as fh:
+    
+with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
@@ -11,7 +8,7 @@ setup(
     version="0.1.0",
     author="Hadrien",
     description="Une application web qui agrège les annonces immobilières",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
